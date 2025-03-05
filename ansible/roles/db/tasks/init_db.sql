b1653f895736e49ec849c96c800282ea5c48bda6
ebenhamu <ebenhamu@gmail.com>
2025-03-05 14:40:57 +0200

    1 table

--- ansible/roles/db/tasks/init_db.sql
+++ ansible/roles/db/tasks/init_db.sql
@@ -1,30 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    domains JSONB DEFAULT '[]'
 );
-
-CREATE TABLE domains (
-    domain_id SERIAL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    domain_name VARCHAR(30) NOT NULL,
-    status_code VARCHAR(10) DEFAULT 'unknown',
-    ssl_expiration VARCHAR(10) DEFAULT 'unknown',
-    ssl_issuer VARCHAR(20) DEFAULT 'unknown',
-    FOREIGN KEY (username) REFERENCES users(username),
-    CONSTRAINT unique_domain_columns UNIQUE (username, domain_name)
-);
-
-INSERT INTO users (username, password) VALUES
-('David', '1223'),
-('Sarah', '4567'),
-('John', '7890'); 
-
-INSERT INTO domains (username, domain_name, status_code, ssl_expiration, ssl_issuer) VALUES
-('David', 'google.com', 'unknown', 'unknown', 'unknown'),
-('David', 'facebook.com', 'unknown', 'unknown', 'unknown'),
-('Sarah', 'yahoo.com', 'unknown', 'unknown', 'unknown'),
-('Sarah', 'bing.com', 'unknown', 'unknown', 'unknown'),
-('John', 'amazon.com', 'unknown', 'unknown', 'unknown'),
-('John', 'apple.com', 'unknown', 'unknown', 'unknown');
